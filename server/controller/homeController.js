ad5a07989ca6b903692c1c1f5b4a5bca3b09d7ac
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-05-21 23:31:46 +0300

    Fix Option in Sign Token

--- server/controller/homeController.js
+++ server/controller/homeController.js
@@ -1,10 +1,6 @@
 const connection = require("../database/config/connection");
 
 const homeController = (req, res) => {
-
-  connection.query('select * from users')
-    .then((data) => console.log(data.rows))
-    .catch((err) => console.log(err));
   res.json({
     error: false,
     data: {
