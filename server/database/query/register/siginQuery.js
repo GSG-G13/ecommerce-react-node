5116266eae7f38335cb74c22b095d5f3d1ee4670
= <shathaamin63@gmail.com>
2023-05-21 14:22:00 +0300

    create signup Query && siginQuery closes #16

---
+++ server/database/query/register/siginQuery.js
@@ -0,0 +1,5 @@
+const connection = require('../../config/connection');
+const signInQuery = (email) => connection.query('SELECT id, name, password as hashed FROM users WHERE email = $1', [email]);
+
+module.exports = signInQuery;
+
