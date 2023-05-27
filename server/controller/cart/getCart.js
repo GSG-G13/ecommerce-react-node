0d974d93a2401a1cca89ca3fe1d424dff342b5d1
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-05-27 20:19:25 +0300

    edit

--- server/controller/cart/getCart.js
+++ server/controller/cart/getCart.js
@@ -1,18 +1,9 @@
 const { getCartQuery } = require('../../database/query');
 
 const getCart = (req, res) => {
-  const { id } = req.user;
-  getCartQuery(id)
+  getCartQuery()
     .then((data) => {
-      const dataWithTotal = data.rows.map((item) => {
-        const totalPrice = item.price * item.quantity;
-        return { ...item, totalPrice };
-      });
-      const total = dataWithTotal.reduce(
-        (acc, item) => acc + item.totalPrice,
-        0,
-      );
-      res.status(200).json({ dataWithTotal, total });
+      res.status(200).json(data.rows);
     })
     .catch((err) => res.status(500).send(err));
 };
