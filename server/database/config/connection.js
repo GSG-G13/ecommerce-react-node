99dff26bd310cdfec9572b5292ebe378ca6e5ed3
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-05-27 22:46:25 +0300

    edit server

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,16 +1,33 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
+require('dotenv').config();
 const { Pool } = require('pg');
 
-const { DATABASE_URL } = process.env;
+const {
+  NODE_ENV, DATABASE_URL, TEST_URL, DEV_URL,
+} = process.env;
+
+let connectionString = '';
+let ssl = false;
+
+switch (NODE_ENV) {
+  case 'production':
+    connectionString = DATABASE_URL;
+    ssl = {
+      rejectUnauthorized: false,
+    };
+    break;
+  case 'development':
+    connectionString = DEV_URL;
+    break;
+  case 'test':
+    connectionString = TEST_URL;
+    break;
+  default:
+    throw new Error('No database is given!!!');
+}
 
 const connection = new Pool({
-  connectionString: DATABASE_URL,
-  ssl: true,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  connectionString,
+  ssl,
 });
 
 module.exports = connection;
